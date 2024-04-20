213736c4b4bebd37843dc5225c75fcd0cb34888b
yousef-15 <115038881+yousef-15@users.noreply.github.com>
2024-04-20 16:16:13 +0200

    Add files via upload

---
+++ Recipe_page/update.js
@@ -0,0 +1,23 @@
+const Search = () => {
+    const searchbox = document.getElementById("search_item").value.toUpperCase();
+    const recipes = document.querySelectorAll(".card");
+
+    recipes.forEach(recipe => {
+        const recipeName = recipe.querySelector('h3');
+        if (recipeName) {
+            const textValue = recipeName.textContent || recipeName.innerHTML;
+            if (textValue.toUpperCase().indexOf(searchbox) > -1) {
+                recipe.style.display = "";
+            } else {
+                recipe.style.display = "none";
+            }
+        }
+    });
+}
+
+
+function go_to_recipe_page(){
+    var image = document.getElementById("image1").src;
+    localStorage.setItem("image", image);
+    window.location.hash = "recipe_page.html";
+}
