e598eeccc80697d4d84a1bb58cebb27c08938ca9
Abdulrahman <147337877+227Abdulrahuman@users.noreply.github.com>
2024-04-21 15:30:09 +0200

    Update script.js

--- Authentication/Signin/script.js
+++ Authentication/Signin/script.js
@@ -20,3 +20,37 @@ const logo = document.getElementById('logo');
 logo.addEventListener('click', function() {
     window.location.href = '../../Home_page/Home Page.html'; 
 });
+const passwordInput = document.getElementById('p');
+let pass = '';
+passwordInput.addEventListener("input", function() {
+    pass =  this.value;
+});
+
+const mailInput = document.getElementById('m');
+let mail = '';
+
+mailInput.addEventListener('input', function() {
+    mail = this.value;
+});
+
+
+
+function login() {
+
+    let x = localStorage.getItem("user");
+
+    let u = JSON.parse(x);
+
+    
+
+    if (mail == u.email && pass == u.pass) {
+        localStorage.setItem("logged", true);
+        alert("Logged in!");
+        window.location = "../../Home_page/Home Page.html";
+    }
+    else {
+        localStorage.setItem("logged", false);
+
+    }
+    
+}
